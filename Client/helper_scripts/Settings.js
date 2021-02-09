95455d3b0b01282512901588ed2e7669aebc0058
Sheikh Salman <sheikhsalman2013@gmail.com>
2021-02-09 16:37:31 +0600

    edited readme

--- Client/helper_scripts/Settings.js
+++ Client/helper_scripts/Settings.js
@@ -2,5 +2,6 @@
         ./Client/helper_scripts/Settings.js
         YOUR DOMAIN OR LOCALHOST:PORT address here
  */
-const proxy='https://tranquil-stream-15974.herokuapp.com/' //leave this empty if don't have any proxyURL like corsanywhere
+//leave proxy empty if don't have any proxyURL like corsanywhere
+const proxy=''
 const Host_domain =proxy+'https://filemanserver.herokuapp.com';
