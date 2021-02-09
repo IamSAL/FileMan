ff9cad17f120696b56d75cc8380d6ccd8dd4cee5
Sheikh Salman <sheikhsalman2013@gmail.com>
2021-02-09 16:39:07 +0600

    ohh cors

--- Client/helper_scripts/Settings.js
+++ Client/helper_scripts/Settings.js
@@ -3,5 +3,5 @@
         YOUR DOMAIN OR LOCALHOST:PORT address here
  */
 //leave proxy empty if don't have any proxyURL like corsanywhere
-const proxy=''
+const proxy='https://tranquil-stream-15974.herokuapp.com/'
 const Host_domain =proxy+'https://filemanserver.herokuapp.com';
